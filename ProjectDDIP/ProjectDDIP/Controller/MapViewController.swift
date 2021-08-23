7e70d85c3b9fbab08726a7bd233f86f264a32dd4
ychaK <petercha2000@gmail.com>
2021-08-24 01:48:41 +0900

    initmapp added

--- ProjectDDIP/ProjectDDIP/Controller/MapViewController.swift
+++ ProjectDDIP/ProjectDDIP/Controller/MapViewController.swift
@@ -16,5 +16,14 @@ class MapViewController: UIViewController {
 		
 		let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
     }
-    
+	
+	private func initMap(_ initialLocation: CLLocation) {
+		self.centerToLocation(initialLocation)
+		
+		let region = MKCoordinateRegion(center: initialLocation.coordinate, latitudinalMeters: 50000, longitudinalMeters: 60000)
+		setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
+		
+		let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+		setCameraZoomRange(zoomRange, animated: true)
+	}
 }
