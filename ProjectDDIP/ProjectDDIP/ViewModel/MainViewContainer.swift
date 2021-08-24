e003127bc9fd6cf1960ffdd4f97a07b321ca0564
su <wkdlfflxh@naver.com>
2021-08-24 13:28:41 +0900

    error fixed

--- ProjectDDIP/ProjectDDIP/ViewModel/MainViewContainer.swift
+++ ProjectDDIP/ProjectDDIP/ViewModel/MainViewContainer.swift
@@ -16,5 +16,6 @@ struct MainViewContainer {
     // guard 처리 하는게 맞는지?
     init(storyboard: UIStoryboard?) {
         mapViewController = storyboard?.instantiateViewController(withIdentifier: "MapViewController") as! MapViewController
+        searchViewController = storyboard?.instantiateViewController(withIdentifier: "SearchViewController") as! SearchViewController
     }
 }
