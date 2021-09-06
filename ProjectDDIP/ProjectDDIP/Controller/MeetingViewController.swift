44686ec2cf2040bc7f9b9e938d9c150fe5095726
su <wkdlfflxh@naver.com>
2021-09-06 17:12:48 +0900

    feat: 버튼 추가 및 위치 변경

--- ProjectDDIP/ProjectDDIP/Controller/MeetingViewController.swift
+++ ProjectDDIP/ProjectDDIP/Controller/MeetingViewController.swift
@@ -8,39 +8,46 @@
 import UIKit
 import MapKit
 
+
 class MeetingViewController: UIViewController {
-	
-	@IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var label: UILabel!
     weak var button: UIButton!
-	var panelUp: () -> Void = {}
+    var panelUp: () -> Void = {}
+    
     
     override func viewDidLoad() {
-      super.viewDidLoad()
-
-      let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
-      button.backgroundColor = .green
-      button.setTitle("Test Button", for: .normal)
-      button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-
-      self.view.addSubview(button)
+        super.viewDidLoad()
+        let height: CGFloat = 50
+        let weight: CGFloat = 100
+        
+        let button = UIButton(frame: CGRect.init(x: (self.view.frame.size.width - weight) / 2,
+                                                 y: (self.view.frame.size.height - height) / 4,
+                                                 width: weight,
+                                                 height: height))
+        button.backgroundColor = .green
+        button.setTitle("Test Button", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
+        self.view.addSubview(button)
     }
-
+    
     @objc func buttonAction(sender: UIButton!) {
-      print("Button tapped")
+        print("Button tapped")
     }
     
 }
 
 extension MeetingViewController: MapViewControllerDelegate {
-	func didUpdateMapVCAnnotation(annotationObject: AnnotationObject) {
-		DispatchQueue.main.async {
-			self.label.text = "\(annotationObject.title!)\n \(annotationObject.locationName!)"
-			self.panelUp()
-
-//			print(annotationObject.locationName as Any)
-//			print(annotationObject.coordinate.latitude)
-//			print(annotationObject.coordinate.longitude)
-//			print(annotationObject.discipline as Any)
-		}
-	}
+    func didUpdateMapVCAnnotation(annotationObject: AnnotationObject) {
+        DispatchQueue.main.async {
+            self.label.text = "\(annotationObject.title!)\n \(annotationObject.locationName!)"
+            self.panelUp()
+            
+            //			print(annotationObject.locationName as Any)
+            //			print(annotationObject.coordinate.latitude)
+            //			print(annotationObject.coordinate.longitude)
+            //			print(annotationObject.discipline as Any)
+        }
+    }
 }
