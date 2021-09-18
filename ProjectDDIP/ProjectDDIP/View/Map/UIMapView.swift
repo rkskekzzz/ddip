8e0e5e7d09728a6c07219a52919829e88a192231
su <wkdlfflxh@naver.com>
2021-09-18 22:29:37 +0900

    feat: add Map

---
+++ ProjectDDIP/ProjectDDIP/View/Map/UIMapView.swift
@@ -0,0 +1,58 @@
+//
+//  UIMapView.swift
+//  ProjectDDIP
+//
+//  Created by su on 2021/09/18.
+//
+
+import SwiftUI
+import MapKit
+import SlideOverCard
+
+struct UIMapView: View {
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 25.7617,
+            longitude: 80.1918
+        ),
+        span: MKCoordinateSpan(
+            latitudeDelta: 10,
+            longitudeDelta: 10
+        )
+    )
+    
+    @State private var position = CardPosition.top
+    @State private var background = BackgroundStyle.blur
+    
+    @State var searchText: String
+    @State var searchResult: [SearchLocation]
+    @State var test: Int
+    
+    var body: some View {
+        ZStack {
+            Map(
+                coordinateRegion: $region,
+                interactionModes: MapInteractionModes.all,
+                showsUserLocation: true,
+                userTrackingMode: $userTrackingMode
+            )
+            
+            SlideOverCard($position, backgroundStyle: $background) {
+                VStack {
+                    SearchBar(searchText: $searchText, searchResult: $searchResult, test: $test)
+                        .padding(.horizontal, 10)
+                }
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct UIMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        UIMapView(searchText: "", searchResult: [], test: 0)
+    }
+    
+}
