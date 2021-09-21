6306f42075257f6c8211b1e907cf3ade4d3c385c
su <wkdlfflxh@naver.com>
2021-09-21 17:40:17 +0900

    feat: something change

--- ProjectDDIP/ProjectDDIP/View/Map/UIMapView.swift
+++ ProjectDDIP/ProjectDDIP/View/Map/UIMapView.swift
@@ -10,9 +10,10 @@ import MapKit
 import SlideOverCard
 
 struct UIMapView: View {
-    @State private var showingScheduleView = false
+    @State private var showingScheduleView:Bool = false
     @State private var position = CardPosition.top
     @State private var background = BackgroundStyle.solid
+//    @State private var region
     
     @State var searchText: String
     @State var searchResult: [SearchLocation]
@@ -25,18 +26,20 @@ struct UIMapView: View {
         } else {
             ZStack {
                 MapView()
+                Button(action: {
+                    self.showingScheduleView.toggle()
+                }) {
+                    Text("go to schedule")
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                .padding(.vertical, 100)
+                .padding(.horizontal, 40)
                 SlideOverCard($position, backgroundStyle: $background) {
                     VStack {
                         SearchView(searchText: searchText, searchResult: searchResult, test: test)
                             .padding(.horizontal, 10)
                             .animation(.default)
                     }
-                }
-
-                Button(action: {
-                    self.showingScheduleView.toggle()
-                }) {
-                    Text("hello")
                 }
             }
             .edgesIgnoringSafeArea(.all)
