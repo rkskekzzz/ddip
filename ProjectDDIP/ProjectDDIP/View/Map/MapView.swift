a8b4e44968f7a4a163f15cb23a203346538012e9
cloudeven <cloudeven@gmail.com>
2021-09-23 17:49:46 +0900

    .:

--- ProjectDDIP/ProjectDDIP/View/Map/MapView.swift
+++ ProjectDDIP/ProjectDDIP/View/Map/MapView.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 import MapKit
 
-protocol MapViewControllerDelegate {
-//    func didUpdateMapVCAnnotation(_ mapViewController: MapViewController, annotationObject: AnnotationObject)
-    func didUpdateMapVCAnnotation(annotationPin: AnnotationPin)
-//    func didUpdateMapVCAnnotationFail(error: Error)
-}
+//protocol MapViewControllerDelegate {
+////    func didUpdateMapVCAnnotation(_ mapViewController: MapViewController, annotationObject: AnnotationObject)
+//    func didUpdateMapVCAnnotation(annotationPin: AnnotationPin)
+////    func didUpdateMapVCAnnotationFail(error: Error)
+//}
 
 struct MapView: UIViewRepresentable {
     typealias UIViewType = MKMapView
@@ -39,7 +39,8 @@ struct MapView: UIViewRepresentable {
 }
 
 extension MapView {
-    class Coordinator: NSObject, MKMapViewDelegate, UIGestureRecognizerDelegate, MapViewControllerDelegate {
+//    class Coordinator: NSObject, MKMapViewDelegate, UIGestureRecognizerDelegate, MapViewControllerDelegate {
+    class Coordinator: NSObject, MKMapViewDelegate, UIGestureRecognizerDelegate {
         
         var parent: MapView
         
@@ -51,15 +52,17 @@ extension MapView {
         
         init(_ parent: MapView) {
             self.parent = parent
+            super.init()
+            self.UIGestureInit()
         }
 
-        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
-            print(mapView.centerCoordinate)
-        }
-        
-        // MapViewControllerDelegate 함수 정의
-        func didUpdateMapVCAnnotation(annotationPin: AnnotationPin) {
-        }
+//        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+//            print(mapView.centerCoordinate)
+//        }
+//
+//        // MapViewControllerDelegate 함수 정의
+//        func didUpdateMapVCAnnotation(annotationPin: AnnotationPin) {
+//        }
         
         // 여기서부터 MKMapViewDelegate 함수 정의
         func mapView(_: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped _: UIControl) {
@@ -133,32 +136,39 @@ extension MapView {
             return MKCoordinateRegion(center: center, latitudinalMeters: latitudinalMeters, longitudinalMeters: longitudinalMeters)
         }
 
-//        func UIGestureInit() {
-//            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(MapViewController.handleTapGesture(gestureRecognizer:)))
-//            mapView.addGestureRecognizer(tapGesture)
-//        }
-//
-//        @objc func handleTapGesture(gestureRecognizer: UITapGestureRecognizer) {
-//            let hitObject = mapView.hitTest(gestureRecognizer.location(in: mapView), with: nil) as? AnnotationView
-//
-//            if mapView.selectedAnnotations.count > 0 {
-//                if hitObject == nil {
-//                    deSelectAnnotation()
-//                    selectedView = nil
-//                    selectedPin = nil
-//                    annotationDeselectBehaviourDefines()
-//                }
-//                return
-//            }
-//            if hitObject != nil { return }
-//
-//            if gestureRecognizer.state == UIGestureRecognizer.State.ended {
-//                let locationCoordinate = mapView.convert(gestureRecognizer.location(in: mapView), toCoordinateFrom: mapView)
-//                gesturePin.resetAttributes("title", "location name", "discipline", locationCoordinate)
-//                replocateAnnotation(gesturePin)
-//                print("Tapped at Latitiude: \(locationCoordinate.latitude), Longitude: \(locationCoordinate.longitude)")
-//            }
-//        }
+        
+        
+        func UIGestureInit() {
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture(gestureRecognizer:)))
+            parent.mapView.addGestureRecognizer(tapGesture)
+        }
+        
+        @objc func handleTapGesture(gestureRecognizer: UITapGestureRecognizer) {
+            let hitTarget = parent.mapView.hitTest(gestureRecognizer.location(in: parent.mapView), with: nil) as? AnnotationView
+            
+            if !parent.mapView.selectedAnnotations.isEmpty {
+                if hitTarget == nil {
+                    releaseFocusedView()
+                    focusedView = nil
+                    selectedPin = nil
+                    
+                    //아래는, 비활성 되었을때 외부에서 할 일이 있으면, 그걸 처리하는 내용을 override할수 있게 정의한 공간이므로
+                    //스테이트머신을 이용하게 되면, 스테이트머신을 업데이트 하는 내용이 들어가고, 아래 내용은 삭제하는편이 좋습니다.
+                    //annotationDeselectBehaviourDefines()
+                }
+                return
+            }
+            
+            if hitTarget != nil { return }
+
+            if gestureRecognizer.state == UIGestureRecognizer.State.ended {
+                
+                let locationCoordinate = parent.mapView.convert(gestureRecognizer.location(in: parent.mapView), toCoordinateFrom: parent.mapView)
+                gesturePin.setAttributes("title", "location name", locationCoordinate)
+                resetAnnotation(pin: gesturePin)
+                print("Tapped at Latitiude: \(locationCoordinate.latitude), Longitude: \(locationCoordinate.longitude)")
+            }
+        }
     }
 }
 
