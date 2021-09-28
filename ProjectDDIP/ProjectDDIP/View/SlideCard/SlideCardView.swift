9334a8a6b3e736a42a01bb6e39284597495cc563
su <wkdlfflxh@naver.com>
2021-09-28 21:22:09 +0900

    fix: merge conflict fixed

--- ProjectDDIP/ProjectDDIP/View/SlideCard/SlideCardView.swift
+++ ProjectDDIP/ProjectDDIP/View/SlideCard/SlideCardView.swift
@@ -18,24 +18,34 @@ enum SlideCardState {
 
 struct SlideCardView: View {
 	@State private var slideCardState: SlideCardState = .none
+    @State private var backgroundStyle = BackgroundStyle.solid
 	
 	@State private var searchViewPosition = CardPosition.bottom
 	@State private var meetingViewPosition = CardPosition.middle
 	
-	var searchViewModel: SearchViewModel
+	var searchViewModel: SearchViewModel = SearchViewModel()
 	var meetingViewModel: MeetingViewModel
 	
 	var body: some View {
-		SlideOverCard($searchViewPosition, backgroundStyle: BackgroundStyle.solid) {
-			SearchView()
-				.padding(.horizontal, 10)
-				.animation(.default)
-		}
-		SlideOverCard($meetingViewPosition, backgroundStyle: BackgroundStyle.solid) {
-			MeetingView()
-				.padding(.horizontal, 10)
-				.animation(.default)
-		}
+        switch slideCardState {
+        case .none:
+            Text("hello")
+        case .search:
+            SlideOverCard($searchViewPosition, backgroundStyle: $backgroundStyle) {
+                SearchView(searchBarPosition: $searchViewPosition, viewModel: searchViewModel)
+                    .padding(.horizontal, 10)
+                    .animation(.default)
+            }
+        case .meeting:
+            SlideOverCard($meetingViewPosition, backgroundStyle: $backgroundStyle) {
+                MeetingView()
+                    .padding(.horizontal, 10)
+                    .animation(.default)
+            }
+        }
+        
+		
+		
 	}
 }
 
