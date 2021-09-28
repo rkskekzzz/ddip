be1c69e5ca90168b3124a63f4743d8a60fb47510
Euimin Chung <jane3079@korea.ac.kr>
2021-09-28 15:00:08 +0900

    refactor: UIMapView를 NavigationView와 NavigationLink를 이용하는 방식으로 수정

--- ProjectDDIP/ProjectDDIP/View/Map/UIMapView.swift
+++ ProjectDDIP/ProjectDDIP/View/Map/UIMapView.swift
@@ -13,34 +13,37 @@ struct UIMapView: View {
     @State private var showingScheduleView:Bool = false
     @State private var searchBarPosition = CardPosition.bottom
     @State private var searchBarBackground = BackgroundStyle.solid
-//    @State private var region
     
     @State var searchText: String
     @State var searchResult: [SearchLocation]
     
     
-    
     var body: some View {
-        if showingScheduleView {
-            ScheduleView(showingScheduleView: $showingScheduleView, mySchedule: EnvironmentObject<MySchedule>())
-                .animation(.spring())
-        } else {
+        NavigationView {
             ZStack {
                 MapView()
-                Button(action: {
-                    self.showingScheduleView.toggle()
-                }, label: {
-                    Image(systemName: "calendar.circle")
-                    
-                })
-                    .font(.system(size: 50))
-                    .foregroundColor(.white)
-                    .background(Color.pink)
-                    .cornerRadius(100)
-                    .shadow(color: Color.pink.opacity(0.3), radius: 20, x: 0, y: 10)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
-                    .padding(.vertical, 100)
-                    .padding(.horizontal, 40)
+                HStack {
+                    NavigationLink(destination: NewMeetingView()) {
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 50))
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(100)
+                            .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 10)
+                    }
+                    NavigationLink(destination: ScheduleView()) {
+                        Image(systemName: "calendar.circle")
+                            .font(.system(size: 50))
+                            .foregroundColor(.white)
+                            .background(Color.pink)
+                            .cornerRadius(100)
+                            .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 10)
+                    }
+                    .buttonStyle(.plain)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                .padding(.vertical, 100)
+                .padding(.horizontal, 40)
                 SlideOverCard($searchBarPosition, backgroundStyle: $searchBarBackground) {
                     VStack {
                         SearchView(searchText: searchText, searchResult: searchResult, searchBarPosition: $searchBarPosition)
@@ -52,7 +55,6 @@ struct UIMapView: View {
             .edgesIgnoringSafeArea(.all)
         }
     }
-    
 }
 
 struct UIMapView_Previews: PreviewProvider {
